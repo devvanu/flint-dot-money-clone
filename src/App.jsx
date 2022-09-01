77d073e5d1e19708aef78fbcba16779596df8c58
Anu Kumari <anukumari21529@gmail.com>
2022-09-01 07:12:18 +0530

    update: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import {Routes, Route} from 'react-router-dom'; 
+
+import Home from './routes/Home';
+import About from './routes/About';
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='about' element={<About />} />
+        </Routes>
+    </>
   )
 }
 
