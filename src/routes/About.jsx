7440e148ad12a0df8ade7a536c83570bfb3e55cf
Anu Kumari <anukumari21529@gmail.com>
2022-09-02 10:26:26 +0530

    update: added Intro to About Us

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,7 +1,18 @@
+import Shades from "../components/Shades";
+import Content from "../components/Content";
+import Intro from "../components/Intro";
+
+import { contentDetails } from "../data";
+
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <Intro 
+        content = {<Content details={contentDetails[1]} />}
+        shades = {<Shades />}
+      />
+    </>
   )
 }
 
-export default About
+export default About;
