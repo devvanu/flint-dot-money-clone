faac060d08f2c7b688abc1dec46fc83ce7ebe494
Anu Kumari <anukumari21529@gmail.com>
2022-09-03 19:35:32 +0530

    style: fix the carousel animation

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,35 @@
-import { keyframes, Box, Image } from "@chakra-ui/react"
-import { motion } from "framer-motion";
+import { Stack, keyframes, Box, Image } from "@chakra-ui/react"
 
 import binance from '../assets/binance.png';
-
+import coinbase from '../assets/coinbase.png';
+import ftx from '../assets/ftx.png';
+import polygon from '../assets/polygon.png';
+import solana from '../assets/solana.png';
 
 const Carousel = () => {
 
     const rotate = keyframes`
-    0% { transform: rotate(0deg) translate(-0px) rotate(0deg) }
-    // 25% { transform: rotate(90deg) translate(-0px) rotate(-90deg) }
-    // 50% { transform: rotate(180deg) translate(-0px) rotate(-1800deg) }
-    // 75% { transform: rotate(270deg) translate(-0px) rotate(-270deg) }
-    // 100% { transform: rotate(360deg) translate(-10px) rotate(-360deg) }
+    0% { transform: translate(7px); }
+    25% { transform: translate(14px); }
+    50% { transform: translate(21px); }
+    75% { transform: translate(14px); }
+    100% { transform: translate(7px); }
   `;
 
-  const animation = `${rotate} 5s linear infinite`;
+  const styles = {
+    pos: 'absolute',
+    boxSize: '80px',
+    animation: `${rotate} 3s linear infinite`
+  }
 
   return (
-    <Box pos='absolute' left='130px' w='100%' h='50%' border='1px dashed grey' borderRadius='50% 60%' transform='rotate(10deg)' >
-        <Box pos='absolute' top='-20px' left='55%' boxSize='50px' bg='red' as={motion.div} animation={animation} />
-        <Box pos='absolute' top='-15px' left='22%' boxSize='50px' bg='yellow' as={motion.div} animation={animation} />
-        <Box pos='absolute' top='35%' left='-25px' boxSize='50px' bg='pink' as={motion.div} animation={animation} />
-        <Box pos='absolute' top='80%' left='20%' boxSize='50px' bg='green' as={motion.div} animation={animation} />
-        <Box pos='absolute' top='90%' left='55%' boxSize='50px' bg='purple' as={motion.div} animation={animation} />
-    </Box>
+    <Stack pos='absolute' left='130px' w='100%' h='50%' border='1px dashed grey' borderRadius='50% 60%' transform='rotate(10deg)' >
+        <Image sx={styles} src={ftx} top='-30px' left='55%' />
+        <Image sx={styles} src={solana} top='-35px' left='22%' />
+        <Image sx={styles} src={binance} top='35%' left='-35px' />
+        <Image sx={styles} src={coinbase} top='75%' left='20%' />
+        <Image sx={styles} src={polygon} top='80%' left='55%' />
+    </Stack>
   )
 }
 
